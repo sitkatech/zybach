be7045c336e226953b0d7702dfbe5b478896595b
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2020-09-01 14:28:07 -0700

    Update variable names and validation for original mock endpoint and add a new mock endpoint

--- Source/Zybach.API/Controllers/WellController.cs
+++ Source/Zybach.API/Controllers/WellController.cs
@@ -9,7 +9,7 @@ using Zybach.Models.DataTransferObjects;
 namespace Zybach.API.Controllers
 {
     [ApiController]
-    public class WaterReportController : ControllerBase
+    public class WellController : ControllerBase
     {
         /// <summary>
         /// Returns a time series representing pumped volume at a well, averaged on a chosen reporting interval, for a given date range.
@@ -17,28 +17,44 @@ namespace Zybach.API.Controllers
         /// </summary>
         /// <param name="wellRegistrationID">The Well Registration ID for the requested Well.</param>
         /// <param name="reportingIntervalMinutes">The reporting interval, in minutes</param>
-        /// <param name="startDateISO">The start date for the report, formatted as "yyyyMMdd"</param>
-        /// <param name="endDateISO">The end date for the report, formatted as "yyyyMMdd"</param>
+        /// <param name="startDateISO">The start date for the report, formatted as an ISO date string with a timezone</param>
+        /// <param name="endDateISO">The end date for the report, formatted as an ISO date string with a timezone</param>
         /// <returns>A time series representing the average pumped volume for the given date range.</returns>
         /// <response code="200">Returns the requested time series</response>
         /// <response code="404">If the requested Well was not found</response>
         /// <response code="400">If the inputs are improperly-formatted or the date range or reporting interval are invalid. Error message will describe the invalid parameter(s)</response>
-        [HttpGet("/waterReport/pumpedVolume/{wellRegistrationID}")]
+        [HttpGet("/well/{wellRegistrationID}/pumpedVolume")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<PumpedVolumeDto> PumpedVolume([FromRoute] string wellRegistrationID,
             [FromQuery] int reportingIntervalMinutes, [FromQuery] string startDateISO, [FromQuery] string endDateISO)
         {
-            var startDate = DateTime.ParseExact(startDateISO, "yyyyMMdd", CultureInfo.InvariantCulture).Date;
-            var endDate = DateTime.ParseExact(endDateISO, "yyyyMMdd", CultureInfo.InvariantCulture).Date;
+            var startDate = DateTime.Parse(startDateISO, null, DateTimeStyles.RoundtripKind).Date;
+            var endDate = DateTime.Parse(startDateISO, null, DateTimeStyles.RoundtripKind).Date;
 
             // todo: implement endpoint
 
             return Ok(new PumpedVolumeDto
             {
-                
+                ReportingIntervalMinutes = reportingIntervalMinutes
             });
+        }
+        /// <summary>
+        /// Returns a dictionary containing key summary statistics for a given well
+        /// </summary>
+        /// <param name="wellRegistrationID">The Well Registration ID for the requested Well</param>
+        /// <returns>A dictionary containing key summary statistics for a given well</returns>
+        /// <response code="200">Returns the requested summary statistics</response>
+        /// <response code="404">If the requested Well was not found</response>
+        [HttpGet("/well/{wellRegistrationID}/summaryStatistics")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<WellSummaryStatisticsDto> SummaryStatistics([FromRoute] string wellRegistrationID)
+        {
+            // todo: implement endpoint
+            return Ok(new WellSummaryStatisticsDto());
         }
     }
 }
--- Source/Zybach.Models/DataTransferObjects/PumpedVolumeDto.cs
+++ Source/Zybach.Models/DataTransferObjects/PumpedVolumeDto.cs
@@ -6,13 +6,19 @@ namespace Zybach.Models.DataTransferObjects
 {
     public class PumpedVolumeDto
     {
-        public PumpedVolumeTimePoint[] AveragePumpedVolumeTimeSeries { get; set; }
-        public decimal PumpingRateGallonsPerMinute { get; set; }
+        public int ReportingIntervalMinutes { get; set; }
+        public PumpedVolumeTimePoint[] PumpedVolumeTimeSeries { get; set; }
+        
     }
 
     public class PumpedVolumeTimePoint
     {
         public DateTime Time { get; set; }
         public decimal PumpedVolumeGallons { get; set; }
+    }
+
+    public class WellSummaryStatisticsDto
+    {
+        public decimal PumpingRateGallonsPerMinute { get; set; }
     }
 }
