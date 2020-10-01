e5608ab359c206b96b982338aeddda1d0ef3e477
Mack Peters <mack.peters@sitkatech.com>
2020-09-30 17:01:21 -0700

    Dummy endpoint with no validation to ensure that secrets aren't holding us back

--- Source/Zybach.NodeAPI/app.js
+++ Source/Zybach.NodeAPI/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('/api/*', checkApiKey);
+app.all('/api/wells/*', checkApiKey);
 app.use('/api', apiRouter);
 
 
--- Source/Zybach.NodeAPI/app_api/routes/index.js
+++ Source/Zybach.NodeAPI/app_api/routes/index.js
@@ -5,4 +5,8 @@ const ctrlWells = require('../controllers/wells');
 //pumpedVolume
 router.route('/wells/:wellid/pumpedVolume').get(ctrlWells.getPumpedVolume)
 
+router.route('/').get((req, res) => {
+    res.status(200).json({status:"success"});
+})
+
 module.exports = router;
