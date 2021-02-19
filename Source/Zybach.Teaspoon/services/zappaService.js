3d34f2b88facfa4cc412514907ad16e637d51310
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2021-02-19 12:38:52 -0800

    Move zappa URL to config in teaspoon

--- Source/Zybach.Teaspoon/services/zappaService.js
+++ Source/Zybach.Teaspoon/services/zappaService.js
@@ -1,20 +1,21 @@
 //const config = require("./config");
-const baseUrl = "https://azwhtugjz2.execute-api.us-east-2.amazonaws.com/prod";
 const { Client } = require('node-rest-client');
 const config = require('../config');
+const baseUrl = config.zappaBaseUrl;
 
 const client = new Client();
 const args = { data: "", headers: { "x-api-key": config.zappaApiKey } };
 
 function getPumpingRate(wellRegistrationID) {
     return new Promise((resolve, reject) => {
-        url = `${baseUrl}/wells/${wellRegistrationID.toUpperCase()}/summaryStatistics`;
+        url = `${baseUrl}/wells/${wellRegistrationID.toUpperCase()}/summary-statistics`;
 
         client.get(url, args, (body, response) => {
             if (response.statusCode == 200) {
-                const pumpingRate = body.wellAuditPumpRate ? body.wellAuditPumpRate :
-                    (body.wellRegisteredPumpRate ? body.wellRegisteredPumpRate :
-                        (body.wellTpnrdPumpRate ? body.wellTpnrdPumpRate : 0)
+                const data = body.data;
+                const pumpingRate = data.wellAuditPumpRate ? data.wellAuditPumpRate :
+                    (data.wellRegisteredPumpRate ? data.wellRegisteredPumpRate :
+                        (data.wellTpnrdPumpRate ? data.wellTpnrdPumpRate : 0)
                     );
                 if (!pumpingRate) {
                     console.error(`No Pumping Rate found for Well ${wellRegistrationID}`);
