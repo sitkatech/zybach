b3a0d2457d67f137bb89e39707f46d253d1af9f2
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2021-06-17 14:49:47 -0700

    Add excel download to sensor status grid

--- Source/Zybach.Web/src/app/pages/sensor-status/sensor-status.component.ts
+++ Source/Zybach.Web/src/app/pages/sensor-status/sensor-status.component.ts
@@ -25,13 +25,33 @@ export class SensorStatusComponent implements OnInit, OnDestroy {
   public wellsObservable: any;
   public wellsGeoJson: any;
   public redSensors: any[];
-  public columnDefs: any[]
+  public columnDefs: any[];
+  public gridApi: any;
 
   constructor(private authenticationService: AuthenticationService,
     private sensorStatusService: SensorStatusService) { }
 
   ngOnInit(): void {
     this.columnDefs = [
+      {
+        headerName: '', valueGetter: function (params: any) {
+          return { LinkValue: params.data.wellRegistrationID, LinkDisplay: "View Well", CssClasses: "btn-sm btn-zybach" };
+        }, cellRendererFramework: LinkRendererComponent,
+        cellRendererParams: { inRouterLink: "/wells/" },
+        comparator: function (id1: any, id2: any) {
+          let link1 = id1.LinkDisplay;
+          let link2 = id2.LinkDisplay;
+          if (link1 < link2) {
+            return -1;
+          }
+          if (link1 > link2) {
+            return 1;
+          }
+          return 0;
+        },
+        width: 160,
+        resizable: true
+      },
       { headerName: 'Well Number', field: 'wellRegistrationID', sortable: true, filter: true, resizable: true },
       { headerName: 'Sensor Number', field: 'sensorName', sortable: true, filter: true, resizable: true},
       { headerName: 'Last Message Age (Hours)', field: 'messageAge', sortable: true, filter: true, resizable: true, valueFormatter: (params) => `${Math.floor(params.value / 3600)} hours` },
@@ -59,7 +79,6 @@ export class SensorStatusComponent implements OnInit, OnDestroy {
         }
 
         this.redSensors = wells.reduce((sensors, well) => sensors.concat(well.sensors.map(sensor => ({ ...sensor, wellRegistrationID: well.wellRegistrationID }))), []).filter(sensor => sensor.messageAge > 3600 * 8);
-console.log(this.redSensors);
 
       })
     });
@@ -68,6 +87,27 @@ console.log(this.redSensors);
   ngOnDestroy(): void {
     this.watchUserChangeSubscription.unsubscribe();
     this.wellsObservable.unsubscribe();
+  }
+  
+  public downloadCsv(){
+
+    // quick and easy way to exclude the column with the "View" buttons from the download:
+    // since it's the first column in the grid, we can just get all columns and ignore the first one
+    const columns = this.gridApi.columnController.getAllGridColumns();
+    const columnsToDownload = columns.slice(1);
+
+
+    // the columnKeys parameter can be either a column object (obtained as above from the grid api)
+    // or a string, set as the "colId" property of a column in the column definitions.
+    // if we ever need to add more columns that won't be intended for download, we'll need to set
+    // colIds on the column definitions and pass an explicit list of the desired ids to exportDataAsCsv,
+    // because the trick we're using here only works since the one column we want to ignore is the first one.
+    this.gridApi.exportDataAsCsv({columnKeys: columnsToDownload});
+  }
+
+  
+  public onGridReady(params) {
+    this.gridApi = params.api;
   }
 }
 
