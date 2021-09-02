ba5f0b8271b6dd645af0c654b75f5f81dfcb1a9a
raynardlee <raynardlee@yahoo.com>
2021-09-01 19:44:25 -0700

    forgot to checkin WellService.cs

---
+++ Source/Zybach.API/Services/WellService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Zybach.EFModels.Entities;
+using Zybach.Models.DataTransferObjects;
+
+namespace Zybach.API.Services
+{
+    public class WellService
+    {
+        private readonly ZybachDbContext _dbContext;
+        private readonly ILogger<WellService> _logger;
+        private readonly GeoOptixService _geoOptixService;
+
+        public WellService(ZybachDbContext dbContext, ILogger<WellService> logger, GeoOptixService geoOptixService)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+            _geoOptixService = geoOptixService;
+        }
+
+        public async Task<List<WellWithSensorSummaryDto>> GetAghubAndGeoOptixWells()
+        {
+            var agHubWells = AgHubWell.GetAgHubWellsAsWellWithSensorSummaryDtos(_dbContext);
+            var wellsWithSensorSummaryFromGeoOptix = await _geoOptixService.GetWellsWithSensors();
+            foreach (var geoOptixWell in wellsWithSensorSummaryFromGeoOptix)
+            {
+                var wellWithSensorSummaryDto =
+                    agHubWells.SingleOrDefault(x => x.WellRegistrationID == geoOptixWell.WellRegistrationID);
+                if (wellWithSensorSummaryDto == null)
+                {
+                    agHubWells.Add(geoOptixWell);
+                }
+            }
+
+            var lastReadingDateTimes = WellSensorMeasurement.GetLastReadingDateTimes(_dbContext);
+            var firstReadingDateTimes = WellSensorMeasurement.GetFirstReadingDateTimes(_dbContext);
+            agHubWells.ForEach(x =>
+            {
+                x.LastReadingDate = lastReadingDateTimes.ContainsKey(x.WellRegistrationID)
+                    ? lastReadingDateTimes[x.WellRegistrationID]
+                    : (DateTime?)null;
+                x.FirstReadingDate = firstReadingDateTimes.ContainsKey(x.WellRegistrationID)
+                    ? firstReadingDateTimes[x.WellRegistrationID]
+                    : (DateTime?)null;
+            });
+
+            return agHubWells;
+        }
+    }
+}
