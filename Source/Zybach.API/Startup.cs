adc560b8bbe16316b267487d194de7b60528fb98
raynardlee <raynardlee@yahoo.com>
2021-11-24 09:45:31 -0800

    * * Add Swashbuckle.AspNetCore package * Add services and middleware required for swagger * Endpoint setup for UI at https://<apihostname>/swagger * Add the build target that generates the static swagger.json file * add gitignore entry for the .config directory that gets added with the dotnet tool * first commit of the generated swagger.json file

--- Source/Zybach.API/Startup.cs
+++ Source/Zybach.API/Startup.cs
@@ -182,6 +182,7 @@ namespace Zybach.API
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+            services.AddSwaggerGenNewtonsoftSupport();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
