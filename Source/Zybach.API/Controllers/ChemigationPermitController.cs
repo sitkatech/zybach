8a2e073243d0979a425d66b67ac25eb7d2eb9f05
Andy Schultheiss <andy.schultheiss@sitkatech.com>
2021-11-23 16:47:51 -0800

    remove commented unused method

--- Source/Zybach.API/Controllers/ChemigationPermitController.cs
+++ Source/Zybach.API/Controllers/ChemigationPermitController.cs
@@ -43,14 +43,6 @@ namespace Zybach.API.Controllers
             return Ok(chemigationPermitsDto);
         }
 
-        //[HttpGet("/api/chemigationPermits/withCurrentDetails")]
-        //[ZybachViewFeature]
-        //public ActionResult<IEnumerable<ChemigationPermitCurrentDetailsDto>> GetAllChemigationPermitsWithCurrentDetails()
-        //{
-        //    var chemigationPermitsWithCurrentDetails = ChemigationPermit.ListWithCurrentDetails(_dbContext);
-        //    return Ok(chemigationPermitsWithCurrentDetails);
-        //}
-
         [HttpGet("/api/chemigationPermits/getByID/{chemigationPermitID}")]
         [ZybachViewFeature]
         public ActionResult<ChemigationPermitDto> GetChemigationPermitByID([FromRoute] int chemigationPermitID)
