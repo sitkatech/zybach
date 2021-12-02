ac1459df540f683e0507f6d23dd32c8e435468e5
raynardlee <raynardlee@yahoo.com>
2021-12-01 16:59:37 -0800

    using list of years hardcoded from 2018-2040 for ExpirationYear; at some pt we can shift it to use an API but it's not that important for now

--- Source/Zybach.Models/DataTransferObjects/ChemigationPermitAnnualRecordApplicatorUpsertDto.cs
+++ Source/Zybach.Models/DataTransferObjects/ChemigationPermitAnnualRecordApplicatorUpsertDto.cs
@@ -8,7 +8,6 @@ namespace Zybach.Models.DataTransferObjects
         public int ChemigationPermitAnnualRecordID { get; set; }
         public string ApplicatorName { get; set; }
         public int CertificationNumber { get; set; }
-        [Range(2020, 2050, ErrorMessage = "Expiration Year needs to be between 2020 and 20250")]
         public int ExpirationYear { get; set; }
         [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Phone numbers must be submitted in 10 digit format with optional hyphens or spaces")]
         public string HomePhone { get; set; }
