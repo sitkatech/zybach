855cf911a46c84c471698001fa66a2d23136d010
Mack Peters <mack.peters@sitkatech.com>
2022-02-02 17:32:51 -0800

    Repurpose SitkaCaptureService for VegaRendererService. Endpoints don't actually work yet but wanted to get changes out

---
+++ Source/VegaRenderService/CapturePostData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class CapturePostData
+    {
+        public string spec { get; set; }
+    }
+}
--- Source/VegaRenderService/VegaRenderService.cs
+++ Source/VegaRenderService/VegaRenderService.cs
@@ -1,40 +1,35 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
-namespace SitkaCaptureService
+namespace VegaRenderService
 {
-    public class SitkaCaptureService
+    public class VegaRenderService
     {
         private static HttpClient _client { get; set; }
         private static ILogger _logger { get; set; }
 
-        public SitkaCaptureService(string baseUri, ILogger logger)
+        public VegaRenderService(string baseUri, ILogger logger)
         {
             _logger = logger;
             _client = new HttpClient()
             {
                 BaseAddress = new Uri(baseUri)
             };
+
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("image/png"));
         }
 
-        public async Task<byte[]> PrintPDF(CapturePostData postData)
+        public async Task<byte[]> PrintPNG(CapturePostData postData)
         {
             _logger.Information($"Handling PrintPDF request with BaseAddress {_client.BaseAddress}");
-            var response = await _client.PostAsJsonAsync("/pdf", postData);
+            var response = await _client.PostAsJsonAsync("/", postData);
             var pdf = response.Content.ReadAsByteArrayAsync();
             return await pdf;
-        }
-
-        public async Task<byte[]> PrintImage(CapturePostData postData)
-        {
-            _logger.Information($"Handling PrintImage request with BaseAddress {_client.BaseAddress}");
-            var response = await _client.PostAsJsonAsync("/image", postData);
-            var image = response.Content.ReadAsByteArrayAsync();
-            return await image;
         }
 
     }
