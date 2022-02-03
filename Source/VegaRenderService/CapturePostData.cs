620123822f9dc839370ef9bae01853abbd3bac50
Mack Peters <mack.peters@sitkatech.com>
2022-02-03 13:40:37 -0800

    Remove some pieces that are no longer used

--- Source/VegaRenderService/CapturePostData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Microsoft.Extensions.DependencyInjection
-{
-    public class CapturePostData
-    {
-        public string spec { get; set; }
-    }
-}
