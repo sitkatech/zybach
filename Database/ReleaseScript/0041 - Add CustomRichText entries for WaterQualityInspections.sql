a46b85f1d7dca4837c46ae286a73a43e2e575c4a
Andy Schultheiss <andy.schultheiss@sitkatech.com>
2022-02-11 14:30:05 -0800

    fixed accidental mod of old release script

--- Database/ReleaseScript/0041 - Add CustomRichText entries for WaterQualityInspections.sql
+++ Database/ReleaseScript/0041 - Add CustomRichText entries for WaterQualityInspections.sql
@@ -1,9 +1,9 @@
 Insert into dbo.CustomRichTextType (CustomRichTextTypeID, CustomRichTextTypeName, CustomRichTextTypeDisplayName)
 values
 
-(18, 'WellInspectionReports', 'Well Inspection Reports')
+(13, 'WaterQualityInspections', 'Water Quality Inspections')
 
 Insert into dbo.CustomRichText(CustomRichTextTypeID, CustomRichTextContent)
 values
 
-(18, 'Default content for: Well Inspection Reports')
+(13, 'Default content for: Water Quality Inspections')
