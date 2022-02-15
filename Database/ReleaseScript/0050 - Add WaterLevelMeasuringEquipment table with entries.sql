7b963769c2ee8886720d8977bb9ead4ec9d36237
Andy Schultheiss <andy.schultheiss@sitkatech.com>
2022-02-15 09:12:04 -0800

    remove identity from equipment ID

--- Database/ReleaseScript/0050 - Add WaterLevelMeasuringEquipment table with entries.sql
+++ Database/ReleaseScript/0050 - Add WaterLevelMeasuringEquipment table with entries.sql
@@ -1,13 +1,13 @@
 create table dbo.WaterLevelMeasuringEquipment
 (
-	WaterLevelMeasuringEquipmentID int not null identity (1,1) constraint PK_WaterLevelMeasuringEquipment_WaterLevelMeasuringEquipmentID primary key,
+	WaterLevelMeasuringEquipmentID int not null constraint PK_WaterLevelMeasuringEquipment_WaterLevelMeasuringEquipmentID primary key,
 	WaterLevelMeasuringEquipmentName varchar(50) not null constraint AK_WaterLevelMeasuringEquipment_WaterLevelMeasuringEquipmentName unique,
 	WaterLevelMeasuringEquipmentDisplayName varchar(50) not null constraint AK_WaterLevelMeasuringEquipment_WaterLevelMeasuringEquipmentDisplayName unique
 )
 
 INSERT INTO dbo.WaterLevelMeasuringEquipment(WaterLevelMeasuringEquipmentName, WaterLevelMeasuringEquipmentDisplayName)
 VALUES
-('Electric', 'Electric'),
-('Fiberglass', 'Fiberglass'),
-('Logger', 'Logger'),
-('Steel', 'Steel')
+(1, 'Electric', 'Electric'),
+(2, 'Fiberglass', 'Fiberglass'),
+(3, 'Logger', 'Logger'),
+(4, 'Steel', 'Steel')
