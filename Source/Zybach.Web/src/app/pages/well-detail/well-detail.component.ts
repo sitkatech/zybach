a158d5527416361c48aadd7659f107aea0927b4c
Andy Schultheiss <andy.schultheiss@sitkatech.com>
2022-02-17 16:18:03 -0800

    remove unused imports and commented-out code

--- Source/Zybach.Web/src/app/pages/well-detail/well-detail.component.ts
+++ Source/Zybach.Web/src/app/pages/well-detail/well-detail.component.ts
@@ -1,15 +1,10 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import moment from 'moment';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { WellService } from 'src/app/services/well.service';
-import { environment } from 'src/environments/environment';
-import { AlertService } from 'src/app/shared/services/alert.service';
-import { Alert } from 'src/app/shared/models/alert';
 import { UserDto } from 'src/app/shared/generated/model/user-dto';
 import { WellDetailDto } from 'src/app/shared/generated/model/well-detail-dto';
 import { InstallationRecordDto } from 'src/app/shared/generated/model/installation-record-dto';
-import { UtilityFunctionsService } from 'src/app/services/utility-functions.service';
 
 @Component({
   selector: 'zybach-well-detail',
@@ -38,9 +33,7 @@ export class WellDetailComponent implements OnInit {
     private authenticationService: AuthenticationService,
     private route: ActivatedRoute,
     private router: Router,
-    private cdr: ChangeDetectorRef,
-    private utilityFunctionsService: UtilityFunctionsService,
-    private alertService: AlertService
+    private cdr: ChangeDetectorRef
   ) {
     // force route reload whenever params change;
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
@@ -61,42 +54,5 @@ export class WellDetailComponent implements OnInit {
       this.cdr.detectChanges();
     })
   }
-
-
-  // getDataSourcesLabel() {
-  //   let plural = true;
-  //   let sensorCount = this.getSensorTypes().size;
-  //   if ((sensorCount == 0 && this.well.HasElectricalData) || (sensorCount == 1 && !this.well.HasElectricalData)) {
-  //     plural = false;
-  //   }
-
-  //   return `Data Source${plural ? "s": ""}: `
-  // }
-  
-  // wellInGeoOptixUrl(): string {
-  //   return `${environment.geoOptixWebUrl}/program/main/(inner:site)?projectCName=water-data-program&siteCName=${this.wellRegistrationID}`;
-  // }
-
-  // getSensorTypes() {
-  //   return new Set(this.well.Sensors.map(sensor => {return sensor.SensorType}));
-  // }
-
-  // getLastReadingDate() {
-  //   if (!this.well.LastReadingDate) {
-  //     return ""
-  //   }
-  //   const time = moment(this.well.LastReadingDate)
-  //   //const timepiece = time.format('h:mm a');
-  //   return time.format('M/D/yyyy');// + timepiece;
-  // }
-
-  // getInstallationDate(installation: InstallationRecordDto) {
-  //   if (!installation.Date) {
-  //     return ""
-  //   }
-  //   const time = moment(installation.Date)
-  //   const timepiece = time.format('h:mm a');
-  //   return time.format('M/D/yyyy ') + timepiece;
-  // }
 
 }
