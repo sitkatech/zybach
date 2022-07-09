99990fd67cbdf789d91caf0cb8362e35d7662559
jamie.quishenberry <jamie.quishenberry@sitkatech.com>
2022-07-08 18:26:25 -0700

    Confirm service added from Upward. Confirmation modal added when user is resolving a support ticket.

--- Zybach.Web/src/app/pages/support-ticket-edit/support-ticket-edit.component.ts
+++ Zybach.Web/src/app/pages/support-ticket-edit/support-ticket-edit.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ConfirmService } from 'src/app/services/confirm.service';
 import { SupportTicketUpsertComponent } from 'src/app/shared/components/support-ticket-upsert/support-ticket-upsert.component';
 import { SupportTicketService } from 'src/app/shared/generated/api/support-ticket.service';
+import { SupportTicketStatusEnum } from 'src/app/shared/generated/enum/support-ticket-status-enum';
 import { SupportTicketDetailDto } from 'src/app/shared/generated/model/support-ticket-detail-dto';
 import { SupportTicketUpsertDto } from 'src/app/shared/generated/model/support-ticket-upsert-dto';
 import { UserDto } from 'src/app/shared/generated/model/user-dto';
@@ -10,6 +12,7 @@ import { UserSimpleDto } from 'src/app/shared/generated/model/user-simple-dto';
 import { Alert } from 'src/app/shared/models/alert';
 import { AlertContext } from 'src/app/shared/models/enums/alert-context.enum';
 import { AlertService } from 'src/app/shared/services/alert.service';
+import internal from 'stream';
 
 @Component({
   selector: 'zybach-support-ticket-edit',
@@ -24,6 +27,7 @@ export class SupportTicketEditComponent implements OnInit {
   public isLoadingSubmit: boolean = false;
   public supportTicketID: number;
   public creatorUser: UserSimpleDto;
+  private initialStatusID: number;
 
   constructor(
     private cdr: ChangeDetectorRef,
@@ -31,7 +35,8 @@ export class SupportTicketEditComponent implements OnInit {
     private router: Router,
     private supportTicketService: SupportTicketService,
     private authenticationService: AuthenticationService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private confirmService: ConfirmService
   ) { }
 
   ngOnInit(): void {
@@ -41,6 +46,8 @@ export class SupportTicketEditComponent implements OnInit {
 
       this.supportTicketService.supportTicketsSupportTicketIDGet(this.supportTicketID).subscribe(supportTicketDetailDto => {
         this.creatorUser = supportTicketDetailDto.CreatorUser;
+        this.initialStatusID = supportTicketDetailDto.Status.SupportTicketStatusID;
+
         this.initializeSupportTicketModel(supportTicketDetailDto);
       });
 
@@ -66,23 +73,34 @@ export class SupportTicketEditComponent implements OnInit {
   }
 
   public onSubmit(editSupportTicketForm: HTMLFormElement): void {
+    if (this.model.SupportTicketStatusID == SupportTicketStatusEnum.Resolved && this.model.SupportTicketStatusID != this.initialStatusID) {
+      
+      const modalContents = "Reminder: Please review the data and create anomaly reports if needed before resolving this ticket.";
+      this.confirmService.confirm({ modalSize: "md", buttonClassYes: "btn-zybach", buttonTextYes: "Save", buttonTextNo: "Cancel", title: "Resolve Ticket", message: modalContents }).then(confirmed => {
+        if (confirmed) {
+          this.SaveSupportTicket(editSupportTicketForm);
+        }
+      });
+
+    } else {
+      this.SaveSupportTicket(editSupportTicketForm);
+    }
+  }
+
+  private SaveSupportTicket(editSupportTicketForm: HTMLFormElement) {
     this.isLoadingSubmit = true;
     this.alertService.clearAlerts();
   
-    this.supportTicketService.supportTicketsSupportTicketIDPut(this.supportTicketID, this.model)
-      .subscribe(response => {
+    this.supportTicketService.supportTicketsSupportTicketIDPut(this.supportTicketID, this.model).subscribe(() => {
+      this.isLoadingSubmit = false;
+      editSupportTicketForm.reset();
+      this.router.navigateByUrl("/support-tickets/" + this.supportTicketID).then(() => {
+        this.alertService.pushAlert(new Alert(`Support ticket updated.`, AlertContext.Success));
+      });
+    }, error => {
         this.isLoadingSubmit = false;
-        editSupportTicketForm.reset();
-        this.router.navigateByUrl("/support-tickets/" + this.supportTicketID).then(() => {
-          this.alertService.pushAlert(new Alert(`Support ticket updated.`, AlertContext.Success));
-        });
+        this.cdr.detectChanges();
       }
-        ,
-        error => {
-          this.isLoadingSubmit = false;
-          this.cdr.detectChanges();
-        }
-      );
+    );
   }
-
 }
