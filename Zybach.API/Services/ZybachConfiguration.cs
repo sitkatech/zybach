df00199bcd63ea2d102cfb761ad8bbc75e88576d
raynardlee <raynardlee@yahoo.com>
2024-01-02 18:49:47 -0800

    removed GEOOPTIX Search hostname

--- Zybach.API/Services/ZybachConfiguration.cs
+++ Zybach.API/Services/ZybachConfiguration.cs
@@ -17,7 +17,6 @@
         public string INFLUX_BUCKET { get; set; }
         public string GEOOPTIX_API_KEY { get; set; }
         public string GEOOPTIX_HOSTNAME { get; set; }
-        public string GEOOPTIX_SEARCH_HOSTNAME { get; set; }
         public string AGHUB_API_BASE_URL { get; set; }
         public string AGHUB_API_BUCKET { get; set; }
         public string AGHUB_API_KEY { get; set; }
